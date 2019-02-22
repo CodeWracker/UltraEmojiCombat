6058d59ab0c7f1367c9ed20c4e20fbc055b8a306
Rodrigo <Rodrigo@DESKTOP-J0CGLUU>
2019-02-21 22:40:57 -0300

    status l0

--- UltraEmojiCombat/src/ultraemojicombat/UltraEmojiCombat.java
+++ UltraEmojiCombat/src/ultraemojicombat/UltraEmojiCombat.java
@@ -10,7 +10,6 @@ public class UltraEmojiCombat {
         l[3] = new Lutador("Dead Code", "Australia", 28, 1.93f, 81.6f, 13, 0, 2);
         l[4] = new Lutador("UFOcobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
         l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
-        l[0].status();
     }
     
 }
